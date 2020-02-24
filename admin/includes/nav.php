b7dd5257a6f0418411a46554e0931d65a5a28e0f
Togrul Jafar <60971206+togrul492@users.noreply.github.com>
2020-02-24 13:21:15 +0400

    edit nav.php
    There are not the main-sidebar class and aside tag into nav.php and I has corrected it for the correct view.

--- admin/includes/nav.php
+++ admin/includes/nav.php
@@ -1,8 +1,9 @@
-<section class="sidebar">
+<aside class="main-sidebar">
+   <section class="sidebar">
       <!-- Sidebar user panel -->
       <div class="user-panel">
         <div class="pull-left image">
-          <img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+          <img src="<?=$tempale_url;?>dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
         </div>
         <div class="pull-left info">
           <p>Alexander Pierce</p>
